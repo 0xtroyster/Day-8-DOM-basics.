89a7b8fcd8ea8719b547fed74ad827917644016c
0xtroyster <12094659+0xtroyster@users.noreply.github.com>
2026-05-23 17:32:55 +0700

    Create input-value.js

---
+++ input-value.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#nameInput");
+const button = document.querySelector("#showButton");
+
+[button.addEventListener(](https://button.addEventListener()"click", function () {
+  console.log("Input value:", [input.value);](https://input.value);)
+});
