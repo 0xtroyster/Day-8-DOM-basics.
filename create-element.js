13dc9aa89280d5cf6cd198dbaa28e29315f5702d
0xtroyster <12094659+0xtroyster@users.noreply.github.com>
2026-05-23 17:32:22 +0700

    Create create-element.js

---
+++ create-element.js
@@ -0,0 +1,3 @@
+const paragraph = [document.createElement(](https://document.createElement()"p");
+paragraph.textContent = "This paragraph was created with JavaScript.";
+[document.body.appendChild(paragraph);](https://document.body.appendChild(paragraph);)
